25aa3216684fecf886bc02e5bd100c5f0567416b
Alexander von Renteln <alexander.vonrenteln@advantest.com>
2014-02-12 10:49:52 +0100

    initial commit

---
+++ spec/spec_helper.rb
@@ -0,0 +1,17 @@
+require 'simplecov'
+
+SimpleCov.start do
+  add_filter '/spec'
+  add_filter '/config'
+end
+
+RSpec.configure do |config|
+  # Use color in STDOUT
+  config.color_enabled = true
+
+  # Use color not only in STDOUT but also in pagers and files
+  config.tty = true
+
+  # Use the specified formatter
+  config.formatter = :documentation # :progress, :html, :textmate
+end
