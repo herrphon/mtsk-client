f53e6ae496bf0c45da421bb3b6502ff12b34054d
aeffle <mail@aeffle.de>
2014-02-14 19:05:47 +0100

    external_request more specified

--- spec/features/external_request_spec.rb
+++ spec/features/external_request_spec.rb
@@ -5,7 +5,7 @@ feature 'External request' do
   it 'should throw an exception when accessed' do
     uri = URI('http://test.de')
 
-    expect{Net::HTTP.get(uri)}.to raise_error
+    expect{ Net::HTTP.get(uri) }.to raise_error(WebMock::NetConnectNotAllowedError)
   end
 
 end
